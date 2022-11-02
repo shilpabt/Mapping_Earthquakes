c15d539f6df19aeb462f55d66f08aa6c58e0555c
shilpabt <110636279+shilpabt@users.noreply.github.com>
2022-11-02 09:48:23 -0500

    Update logic.js

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -20,9 +20,9 @@ let cityData = cities;
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
   console.log(city)
-  L.circleMarker(city.location),{
+  L.circleMarker(city.location,{
     radius: city.population/100000
-  }
+  })
   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
 .addTo(map);
 });
@@ -31,10 +31,12 @@ cityData.forEach(function(city) {
 //Leaflet doesn't provide a tile layer. Instead, it offers various tile layer APIs.
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     maxZoom: 18,
     accessToken: API_KEY,
+    tileSize: 512,
+    zoomOffset: -1
     
-});
+}).addTo(map);
 // Then we add our 'graymap' tile layer to the map.streets.addTo(map);
-streets.addTo(map);
+//streets.addTo(map);
